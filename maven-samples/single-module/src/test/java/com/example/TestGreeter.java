6ab3cc4463a35dc4a50262a2b34737d30608386f
Wai Yan Win Htain <waiyanwinhtain2016@gmail.com>
2021-07-09 20:37:48 +0630

    mod: test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -31,9 +31,16 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Just Thanks you.";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
