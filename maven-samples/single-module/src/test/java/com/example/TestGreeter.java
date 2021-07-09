e1ed4c1f141c7606c4b974cea19abce7f5b75794
Wai Yan Win Htain <waiyanwinhtain2016@gmail.com>
2021-07-09 20:39:07 +0630

    mod: test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingThanks() {
     String someone = "World, Just Thanks you.";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
